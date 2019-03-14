7240bc88128c85a8e0b3d1be902e9c7811a256d9
Renata Moura <101096098@georgebrown.ca>
2019-03-14 14:32:40 -0400

    resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -9,110 +9,120 @@ class Resume extends Component{
             <div style={{ width:'65%', margin: 'auto', backgroundColor:'#f0f4c3', padding: '25px'}}>
               
               <span>
-           <h4 style={{textAlign:'center'}}>   Renata Vieira Moura, PMP</h4>
-           <p> _____________________________________</p>
-                1464A Dufferin Street, 
-                Toronto M6H 3L2– Ontario – Canada 
-                phone:  (647) 575-0922 
-                email:  renata.moura45@yahoo.com
+               <h4 style={{textAlign:'center'}}><b>   Renata Vieira Moura, PMP</b></h4>          
+                <p>1464A Dufferin Street, Toronto M6H 3L2– Ontario – Canada </p>
+                <p>  phone:  (647) 575-0922 </p>
+                <p> email:  renata.moura45@yahoo.com</p>
 
               </span>
-              <br/>
-               <span>
-                   
-                    <h5>OBJECTIVE: Project Manager</h5>
-
-                    </span>
-                  <span>  
-                      <p> <h5> SKILLS & COMPETENCIES:</h5></p>
-                   <p> ●	Over 10 years of experience in project management, developing the following areas: 
-                       network implementation, quality, focus on largest Telecommunication Providers, 
-                       controlling cost and budget.</p>
-                   <p>●	Over 22 years of experience in the telecommunication industry acquiring successful 
-                       working relationships with vendors and providers.</p>
-                   <p>  ●	Effective project control activities, such as projects plans, 
-                       weekly status reports, financial reports, meeting minutes, internal and external reports.</p>
-                   <p>●	Ability to competently interact with customers and end users, managing customer expectations, 
-                       focus on result, and utilize strong problem-solving skills and decision making.</p>
-                   <p>●	Manage multiple complex projects simultaneously, proficiency in multitasking and prioritize.</p>
-                   <p>●	Project documentation proficiency gained as a project manager, strong leadership and negotiation
-                        skills and ability to work in an adverse environment.</p>                       
-                   <p>●	Proven effective communication and presentation skills.</p>
-                   <p>●	Computer proficiency with Word, Excel, PowerPoint, Project, Visual Studio and Visio.</p>
-                   <p>●	Excellent communication skills in English, Portuguese and Spanish both written and verbal. </p>
-                   
-                  </span>
+  
+            <span>
+                <h6><b>OBJECTIVE: Project Manager</b></h6>
+            </span>
+            <span>  
+               <p> <h6> SKILLS & COMPETENCIES:</h6></p>
+               <ul>
+                <li> Over 10 years of experience in project management, developing the following areas: 
+                   network implementation, quality, focus on largest Telecommunication Providers, 
+                    controlling cost and budget.</li>
+                <li>Over 22 years of experience in the telecommunication industry acquiring successful 
+                   working relationships with vendors and providers.</li>
+                <li>Effective project control activities, such as projects plans, 
+                   weekly status reports, financial reports, meeting minutes, internal and external reports.</li>
+                <li>Ability to competently interact with customers and end users, managing customer expectations, 
+                    focus on result, and utilize strong problem-solving skills and decision making.</li>
+                <li>Manage multiple complex projects simultaneously, proficiency in multitasking and prioritize.</li>
+                <li>Project documentation proficiency gained as a project manager, strong leadership and negotiation
+                    skills and ability to work in an adverse environment.</li>                
+                <li>Proven effective communication and presentation skills.</li>
+                <li>Computer proficiency with Word, Excel, PowerPoint, Project, Visual Studio and Visio.</li>
+                <li>Excellent communication skills in English, Portuguese and Spanish both written and verbal. </li>
+                </ul>
+            </span>
 
     <br/>
-                <span> 
-                    <p> <h5> Education: </h5></p>
-
-                    <p>●	Computer Programmer Analyst Program – George Brown – Toronto Ontario Anticipated - May 2019 </p>
+            <span> 
+                <p> <h6> Education: </h6></p>
+                <ul>
+               <li>	Computer Programmer Analyst Program – George Brown – Toronto Ontario Anticipated - May 2019 </li>
                     <p> Projects: </p>
-                    <Table>
-                        <TableHead>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>• Recipe Catalogue - HTML</TableCell>
+                                    <TableCell>• Central database for GBC employees</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                    <TableCell>• Grocery Store	-.Net </TableCell>
+                                    <TableCell> • 4r’s – Reduce, Reuse, Recycle, Rewards, Contact list – PHP </TableCell></TableRow>
+                                <TableRow>
+                                <TableCell>	• Game Store – Full Stack</TableCell>
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+
+                <li>	MBA: Executive Marketing MBA - ESPM – Escola Superior de Propag. e Marketing - Dec 2002</li>
+                <li>	Bachelor: Electrical Engineering - Pontifícia Universidade Católica de Minas Gerais  - Jul 1996</li>
+            </ul>
+            </span>
+             <span> 
+                <p> <h6>CERTIFICATIONS:</h6></p>
+                <Table>
+                    <TableHead>
                         <TableRow>
-                            <TableCell>• Recipe Catalogue - HTML</TableCell>
-                            <TableCell>• Central database for GBC employees</TableCell>
-                            </TableRow>
-                            <TableRow>
-                            <TableCell>• Grocery Store	-.Net </TableCell>
-                            <TableCell> • 4r’s – Reduce, Reuse, Recycle, Rewards, Contact list – PHP </TableCell></TableRow>
-                            <TableRow>
-                            <TableCell>	• Game Store – Full Stack</TableCell>
+                            <TableCell>•Project Manager Professional - PMP</TableCell>
+                            <TableCell>•Cisco - IT Essentials</TableCell>
                         </TableRow>
-                        </TableHead>
-                    </Table>
+                    </TableHead>
+                </Table>
+            </span>
+<span>
+<p> <h6> <b>WORK EXPERIENCE:</b></h6></p>
 
-                </span>
-                <br/>
-                <p> ●	MBA: Executive Marketing MBA - ESPM – Escola Superior de Propag. e Marketing - Dec 2002</p>
-                <p> ●	Bachelor: Electrical Engineering - Pontifícia Universidade Católica de Minas Gerais  - Jul 1996</p>
-                <br/>
-                <span> 
-                    <p> <h5>CERTIFICATIONS:</h5></p>
-                    <p> ●	Project Manager Professional - PMP</p>
-                    <p> ●	Cisco - IT Essentials </p>
-                </span>
+    <p><b>Key Holder,</b> Bath and Body works, Toronto, Ontario, Canada 	</p>    
+    <p>		 Mar 2017 to Current</p>
+    <ul>
+       <li>	As part of management team. my responsibilities include training and coaching associate team, 
+                attaining sales goals through a positive customer experience. </li>
+       <li>	Responsible for opens and closes the store, and set the daily plan</li>
+       <li>	Identified customer needs and up sold to higher value products as often as possible</li>
+       <li>	Approach browsing customers and initiate conversations to determine buying preferences</li>
+       <li>	Recommend appropriate items and direct or lead customer to selection</li>
+       <li>	Inquire about buying experience, item preference, and future purchase</li>
+       <li>	Sign customer up for marketing list and inform of upcoming promotion or customer events</li>
+</ul>
 
-<p> <h5> WORK EXPERIENCE:</h5></p>
-<p><b>
-Key Holder,</b> Bath and Body works, Toronto, Ontario, Canada 	</p>    
-<p>		 Mar 2017 to Current</p>
-<p>●	As part of management team. my responsibilities include training and coaching associate team, 
-    attaining sales goals through a positive customer experience. </p>
-    <p>●	Responsible for opens and closes the store, and set the daily plan</p>
-    <p>●	Identified customer needs and up sold to higher value products as often as possible</p>
-    <p>●	Approach browsing customers and initiate conversations to determine buying preferences</p>
-    <p>●	Recommend appropriate items and direct or lead customer to selection</p>
-    <p>●	Inquire about buying experience, item preference, and future purchase</p>
-    <p>●	Sign customer up for marketing list and inform of upcoming promotion or customer events</p>
-
-<p><b> Start-up Consultant,</b> LF Consultoria e Equipamentos LTDA, Belo Horizonte, MG,Brazil   </p>
+    <p><b> Start-up Consultant,</b> LF Consultoria e Equipamentos LTDA, Belo Horizonte, MG,Brazil   </p>
     <p>   Aug 2015 - Jul 2016</p>
-    <p>●	Provided administrative and technical support, treated documents according to administrative process, 
-        and prepared reports</p>
-    <p>●	Established contracts, agreements, and invitation letters and developed projects to improve services</p>
-
- <p>  <b>Customer Project Manager</b>, Ericsson, São Paulo, SP, Brazil           </p>                  
- <p> Oct 2010 - Jun 2015 </p>
-    <p>●	Responsible for large projects such as Implementation of transport network - Microwave (MW) /
-    Synchronous Digital Hierarchy (SDH) - and access network (3G/LTE) at Brazilians operators such as Vivo, 
-    Oi/Telemar, Tim and Claro</p>
-    <p>●	International Experience: Project manager at both Movistar Chile, responsible for the CORE network, 
-    and at Claro Argentina, responsible for transmission network - Microwave (MW) / Synchronous Digital Hierarchy (SDH)</p>
-    <p>●	Provided PMO Service to improve operational efficiency and savings expected for sharing (Oi & Tim)</p>
-    <p>●	Effectively worked with cross functional teams, delivered high level quality projects, achieved 
-    project goals such as time, scope and cost, and successfully identified and prioritized customer needs</p>
-    <p>●	Acted as prime contact towards the customer, controlled and planned time, scope, 
-    cost and quality of delivery for the entire project</p>
-    <p>●	Responsible for project documentation, status report, and risk management</p>
-
-Customer Project Manager, NEC Brazil, Rio de Janeiro, RJ, Brazil                                    Sep 2009 – Jul 2010
-●	Responsible for large projects such as Implementation of 1200 Microwaves(MW) links for Claro, in Brazil area and Synchronous Digital Hierarchy(SDH) expansion 7+1 to 14+2 for Embratel in Manaus.
-●	Effectively worked with cross functional teams, successfully delivered quality projects, and exceeded project goals and expectations
-●	Acted as the prime contact towards customers, controlled and planned all aspects of projects including; tasks, resources, timely delivery, scope, cost and quality 
-●	Responsible for project documentation, status report, and risk management
+    <ul>
+       <li>	Provided administrative and technical support, treated documents according to administrative process, 
+            and prepared reports</li>
+       <li>	Established contracts, agreements, and invitation letters and developed projects to improve services</li>
+    </ul>
+    <p>  <b>Customer Project Manager</b>, Ericsson, São Paulo, SP, Brazil           </p>                  
+    <p> Oct 2010 - Jun 2015 </p>
+    <ul>
+       <li>	Responsible for large projects such as Implementation of transport network - Microwave (MW) /
+        Synchronous Digital Hierarchy (SDH) - and access network (3G/LTE) at Brazilians operators such as Vivo, 
+        Oi/Telemar, Tim and Claro</li>
+       <li>	International Experience: Project manager at both Movistar Chile, responsible for the CORE network, 
+        and at Claro Argentina, responsible for transmission network - Microwave (MW) / Synchronous Digital Hierarchy (SDH)</li>
+       <li>	Provided PMO Service to improve operational efficiency and savings expected for sharing (Oi & Tim)</li>
+       <li>	Effectively worked with cross functional teams, delivered high level quality projects, achieved 
+        project goals such as time, scope and cost, and successfully identified and prioritized customer needs</li>
+       <li>	Acted as prime contact towards the customer, controlled and planned time, scope, 
+        cost and quality of delivery for the entire project</li>
+       <li>	Responsible for project documentation, status report, and risk management</li>
+    </ul>
+    <p>  <b>Customer Project Manager,</b> NEC Brazil, Rio de Janeiro, RJ, Brazil </p>
+        <p> Sep 2009 – Jul 2010</p>
+        <p> ●	Responsible for large projects such as Implementation of 1200 Microwaves(MW) links for Claro,
+            in Brazil area and Synchronous Digital Hierarchy(SDH) expansion 7+1 to 14+2 for Embratel in Manaus.</p>
+        <p> ●	Effectively worked with cross functional teams, successfully delivered quality projects, 
+            and exceeded project goals and expectations</p>
+        <p> ●	Acted as the prime contact towards customers, controlled and planned all aspects of projects including; 
+            tasks, resources, timely delivery, scope, cost and quality </p>
+        <p> ●	Responsible for project documentation, status report, and risk management</p>
 
 Consultant, Promon Logicallis, Rio de Janeiro, RJ, Brazil 	                                       May 2009 - Sep 2009
 ●	Project Manager Consultant. Acting in Claro, under responsibility of engineering and implementation project management in GPRS technology
@@ -142,7 +152,7 @@ Senior Engineer, NEC Brazil, São Paulo, SP, Brazil		                           
 Engineer, Oi/Telemar, Belo Horizonte, MG, Brazil		                                        Nov 1996 - Sep 2000
 ●	Controlled multiple facilities and SDH management, analyzed schematic transmission (FDM/PCM), and examined technical documentation of transmission and commute systems
 ●	Management system SDH (BOSCH, NEC) and SDH Configuration
-
+</span>
 <span>
 Volunteer Experience:                                                                                	 	                  Dec 2016
 ●	York West Active Living Centre – Kitchen Assistant, served lunch on Saturday, organized the kitchen at the end of the service. Interacted with many individuals and developed communication and interpersonal skills
